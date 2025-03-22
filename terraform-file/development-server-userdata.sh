9cd3c8103cce25bef83c2b4f77fad72c486735df
Tahir Mehraj <mehrajtahir@gmail.com>
2025-03-22 19:17:39 +0000

    Fixed the docker compose version

--- terraform-file/development-server-userdata.sh
+++ terraform-file/development-server-userdata.sh
@@ -5,9 +5,9 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
--o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+sudo curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 yum install git -y
 yum install java-11-amazon-corretto -y
 git clone https://github.com/cmakkaya/microservices-with-db-on-dev-server.git
